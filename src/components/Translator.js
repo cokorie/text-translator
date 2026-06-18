ff0461c2a4413ce44ae8ab91b3400f8d940612ba
cokorie <chijioke.okorie@gmail.com>
2026-06-17 19:07:04 -0500

    add toText selector

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -3,6 +3,7 @@ import React, { use, useEffect } from "react";
 const Translator = () => {
     useEffect(() => {
         const fromText = document.querySelector(".from-text");
+        const toText = document.querySelector(".to-text");
     }, []);
 
     return (
