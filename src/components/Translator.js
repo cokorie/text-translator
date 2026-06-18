066d129ab3b584f69413aaa0d2e5bd34c526ccb3
cokorie <chijioke.okorie@gmail.com>
2026-06-17 19:06:34 -0500

    add fromText selector

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { use, useEffect } from "react";
 
 const Translator = () => {
+    useEffect(() => {
+        const fromText = document.querySelector(".from-text");
+    }, []);
+
     return (
         <div>
             <h1>Translator Component</h1>
