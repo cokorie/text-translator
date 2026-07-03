ba6fbfd6ab88ac7626878100e34b156d1f90c7de
cokorie <chijioke.okorie@gmail.com>
2026-07-03 18:39:04 -0500

    add container class

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -82,8 +82,10 @@ const Translator = () => {
             <img src="https://img.icons8.com/ios/50/000000/google-translate.png" className="above-container-content" alt="translate" />
             <h1 className="above-container-content">Translator</h1>
         </div>
-        </>
-
         
+        <div className="container">
+            
+        </div>
+        </>       
     );
 }
