b9a66b8b3a5c0395a35cd1e37b1d4a19856145a9
cokorie <chijioke.okorie@gmail.com>
2026-07-03 14:48:25 -0500

    add icon utterance if-else statements

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -63,6 +63,13 @@ const Translator = () => {
                     }
             } else {
                 let utterance;
+                if (target.id === "from") {
+                    utterance = new SpeechSynthesisUtterance(fromText.value);
+                    utterance.lang = selectTag[0].value;
+                } else {
+                    utterance = new SpeechSynthesisUtterance(toText.value);
+                    utterance.lang = selectTag[1].value;
+                }
             }
         });
 
