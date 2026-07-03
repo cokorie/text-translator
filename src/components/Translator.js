13d9b16178d10234b5454deb7f86b5d149ff505c
cokorie <chijioke.okorie@gmail.com>
2026-07-03 14:17:54 -0500

    add country code output

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -12,7 +12,8 @@ const Translator = () => {
         selectTag.forEach((tag, id) => {
             for (const country_code in countries) {
                 let selected = id === 0 ? country_code === "en-GB" ? "selected" : "" : country_code === "hi-IN" ? "selected" : "";
-                
+                let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+                tag.insertAdjacentHTML("beforeend", option);
             }
         })}
     }, []);
